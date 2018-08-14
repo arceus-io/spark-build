d0a230ee94d7d3608baa91969edba45eb9aaf7d1
Sam Tran <stran@mesosphere.com>
2018-08-14 15:54:50 -0700

    Add run perms to tempfile

--- spark-testing/spark_utils.py
+++ spark-testing/spark_utils.py
@@ -150,6 +150,7 @@ def submit_job(
         args_file = tempfile.NamedTemporaryFile("w")
         args_file.write('dcos spark run {} --submit-args="{}"'.format(verbose_flag, submit_args))
         args_file.flush() # Ensure content is available for CLI to read
+        os.chmod(args_file.name, 0777)
         LOGGER.info("Running subprocess on file {}".format(args_file.name))
         
         result = subprocess.run(args_file.name, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
